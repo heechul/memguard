5f288236bca88ee4f1849f257677893825d356e1
Heechul Yun <heechul@illinois.edu>
2013-07-10 14:46:30 -0500

    PhD defense setting

--- exp-hrt.sh
+++ exp-hrt.sh
@@ -2,11 +2,10 @@
 . functions
 
 outputfile=hrt.txt
-TMPFILE=/run/out.txt
 
 init_system
 rmmod memguard
-set_cpus "1 1 1 0"
+set_cpus "1 1 0 0"
 enable_prefetcher
 
 log_echo "llc:$llc_miss_evt arch:${archbit}bit"
@@ -21,33 +20,33 @@ do_hrt_test()
 {
     local master_schedopt="$2"
     local corun_schedopt="$3"
+    local TMPFILE=$4
     killall -2 thr hrt bandwidth latency
     killall -9 cpuhog
-    if [ ! "$1" = "solo" ]; then
-	log_echo "co-run"
-	./matrix -c 1 -f bwlog.c1&
-	#./cpuhog -c 1 &
-	#./thr -c 1 $corun_schedopt -t 1000000 -f bwlog.c1 &
-	# ./thr -c 2 $corun_schedopt -t 1000000 -f bwlog.c2 &
-	# ./thr -c 3 $corun_schedopt -t 1000000 -f bwlog.c3 &
+    if [ "$1" = "cpuhog" ]; then
+	log_echo "cpuhog"
+	./cpuhog -c 1 $corun_schedopt &
+    elif [ "$1" = "thr" ]; then
+	log_echo "co-run w/ 'thr'"
+	./thr -c 1 $corun_schedopt -t 1000000 -f bwlog.c1 &
+    elif [ "$1" = "xorg" ]; then
+	log_echo "shoud print to screen"
     fi
     # echo "[start]" > /sys/kernel/debug/tracing/trace_marker
     # echo "[finish]" > /sys/kernel/debug/tracing/trace_marker
 
     trace-cmd record -e sched:sched_switch \
-	./hrt -c 2 -i 1000 -C 12 -I 10 $master_schedopt > $TMPFILE \
+	./hrt -c 0 -i 1000 -C 12 -I 10 $master_schedopt > $TMPFILE \
 	|| error "exec failed"
     killall -2 thr hrt bandwidth latency cpuhog matrix
     killall -9 cpuhog
     print_settings
     sleep 1
-    ./printstat.py --deadline=13 $TMPFILE >> $outputfile
+    ./printstat.py --deadline=14 $TMPFILE >> $outputfile
 
-    if [ ! "$1" = "solo" ]; then
+    if [ "$1" = "thr" ]; then
 	bwsum=0
 	bwsum=`expr $bwsum + $(parse_bw_log bwlog.c1)`
-	#bwsum=`expr $bwsum + $(parse_bw_log bwlog.c2)`
-	#bwsum=`expr $bwsum + $(parse_bw_log bwlog.c3)`
 	log_echo "Aggr.B/W: $bwsum"
 	rm bwlog.*
     fi
@@ -58,32 +57,21 @@ test_isolation()
 {
     rmmod memguard
 
-#    log_echo "w/o memguard"
-#    do_hrt_test solo
-#    do_hrt_test co-run
-
     log_echo ">> no memguard"
-    do_hrt_test solo "-o fifo"  "-o normal"
-    #do_hrt_test co-run "-o fifo"  "-o normal"
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-org-solo.txt >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-org.txt
 
-    log_echo ">> memguard(excl2)"
-    do_init_mb "100 200 900" 1 2  >& /dev/null
-    do_hrt_test solo "-o fifo"  "-o normal"
-    #do_hrt_test co-run "-o fifo"  "-o normal"
+    log_echo ">> memguard(excl0)"
+    do_init_mb "900 200" 0 0  >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-excl0-solo.txt >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-excl0.txt
 
     log_echo ">> memguard(excl5)"
-    do_init_mb "100 200 900" 1 5  >& /dev/null
-    do_hrt_test solo "-o fifo"  "-o normal"
+    do_init_mb "900 200" 1 5  >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-excl5-solo.txt >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" /run/out-excl5.txt
 
-    #do_hrt_test co-run "-o fifo"  "-o normal"
     rmmod memguard
-
-#    do_hrt_test co-run "-o normal"  "-o batch"
-#    do_init_share "3 1 1 1" 1 $RMIN 5  >& /dev/null
-#    do_hrt_test co-run
-
-#    do_init_share "9 1 1 1" 1 $RMIN 5  >& /dev/null
-#    do_hrt_test co-run
 }
 
 test_isolation
