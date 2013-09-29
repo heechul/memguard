7af1c8f4877ad55052592e13038139a2c6f2e111
Heechul Yun <heechul@illinois.edu>
2013-09-29 15:56:21 -0500

    hrt: test all 4 hw

--- exp-hrt.sh
+++ exp-hrt.sh
@@ -20,6 +20,7 @@ do_hrt_test()
     local corun_schedopt="$3"
     local FILENAME=$4
     local TMPFILE=/run/$FILENAME
+
     killall -2 thr hrt bandwidth latency
     killall -9 cpuhog
     if [ "$1" = "cpuhog" ]; then
@@ -34,9 +35,12 @@ do_hrt_test()
     # echo "[start]" > /sys/kernel/debug/tracing/trace_marker
     # echo "[finish]" > /sys/kernel/debug/tracing/trace_marker
 
+    echo $$ > /sys/fs/cgroup/tasks
     # trace-cmd record -e sched:sched_switch \
-	./hrt -c 0 -i 1000 -C 12 -I 10 $master_schedopt > $TMPFILE \
+    ./hrt -c 0 -i 1000 -C 12 -I 20 $master_schedopt > $TMPFILE \
 	|| error "exec failed"
+    echo $$ > /sys/fs/cgroup/system/tasks
+
     killall -2 thr hrt bandwidth latency cpuhog matrix
     killall -9 cpuhog
     print_settings
@@ -63,38 +67,55 @@ test_isolation()
     hw=$1
 
     log_echo "^^ no memguard"
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-org-solo >& /dev/null
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-org-corun
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-org-solo >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-org-corun
 
     log_echo "^^ memguard(excl0)"
     do_init_mb "$2 $3" 0 0  >& /dev/null
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-excl0-solo >& /dev/null
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-excl0-corun
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-excl0-solo >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-excl0-corun
 
     log_echo "^^ memguard(excl5)"
     do_init_mb "$2 $3" 1 5  >& /dev/null
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-excl5-solo >& /dev/null
-    do_hrt_test xorg "-o fifo" "-o normal" $hw-out-excl5-corun
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-excl5-solo >& /dev/null
+    do_hrt_test xorg "-o fifo" "-o normal" $hw-$2_$3-out-excl5-corun
 
     rmmod memguard
 }
 
+set_cpus "1 1 1 1"
+# create system partition 
+init_cgroup
+
 # case 1: private, no prefetcher
 echo 2 > /sys/fs/cgroup/system/cpuset.cpus
+
 set_cpus "1 0 1 0"
 disable_prefetcher
+init_cgroup
 test_isolation "hw1" 550 550
 
 # case 2: private, prefetcher
+echo 2 > /sys/fs/cgroup/system/cpuset.cpus
 set_cpus "1 0 1 0"
 enable_prefetcher
+init_cgroup
 test_isolation "hw2" 550 550
+test_isolation "hw2" 900 200
 
 # case 3: shared, prefetcher
-set_cpus "1 1 0 0"
+set_cpus "1 1 1 1"
 echo 1 > /sys/fs/cgroup/system/cpuset.cpus
+set_cpus "1 1 0 0"
 enable_prefetcher
+init_cgroup
 test_isolation "hw3" 900 200
+
+# case 4: shared, no prefetcher
+set_cpus "1 1 0 0"
+disable_prefetcher
+init_cgroup
+test_isolation "hw4" 900 200
 
 finish
 rmmod memguard
