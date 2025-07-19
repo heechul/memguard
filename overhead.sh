81b5077ff62d116d63e1f489d52449ce1b358fa9
Heechul Yun <heechul.yun@ku.edu>
2025-07-19 16:39:17 -0500

    Test more fine-grained periods
    also change the default scheduler from FIFO to CFS with nice -20

--- overhead.sh
+++ overhead.sh
@@ -4,13 +4,13 @@ if [ "$(id -u)" != "0" ]; then
 	echo "This script must be run as root" 1>&2
 	exit 1
 fi
-base_bw=`bandwidth -c 0 -r 1 -t 5 -x 2>/dev/null | grep "B/W" | awk '{ print $4 }'`
+base_bw=`bandwidth -c 0 -p -20 -t 10 -x 2>/dev/null | grep "B/W" | awk '{ print $4 }'`
 echo "none" 1.0 $base_bw
 
 # Test overhead of memguard with different periods
-for p in 50 100 200 500 1000 10000; do
-	insmod memguard.ko g_period_us=$p g_read_budget_mb=20000 g_write_budget_mb=20000
-	bw=`bandwidth -c 0 -r 1 -t 5 -x 2> /dev/null | grep "B/W" | awk '{ print $4 }'`
+for p in 10 20 50 100 200 500 1000 10000; do
+	insmod memguard.ko g_period_us=$p g_read_budget_mb=900000 g_write_budget_mb=900000
+	bw=`bandwidth -c 0 -p -20  -t 10 -x 2> /dev/null | grep "B/W" | awk '{ print $4 }'`
 	slowdown=`echo "scale=2; $base_bw / $bw" | bc`
 	echo $p $slowdown $bw
 	rmmod memguard
